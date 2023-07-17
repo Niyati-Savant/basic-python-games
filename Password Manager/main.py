bb08ad417791b6cde2b622a1bfe3d0c2ac7d8af6
Niyati Savant <CodingTerex18@gmail.com>
2023-07-17 17:26:07 +0530

    Add a search functionality and update the data file to json

--- Password Manager/main.py
+++ Password Manager/main.py
@@ -1,8 +1,8 @@
 from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
+import json
 import pyperclip
-
 
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
            'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
@@ -23,11 +23,33 @@ def generate_pass():
     pyperclip.copy(password)
 
 
+# ---------------------------- PASSWORD FINDER ------------------------------- #
+def find_password():
+    user_needs = website_entry.get()
+    user_needs = user_needs.title()
+    try:
+        with open(file="Password_details.json", mode='r') as data:
+            file_contents = json.load(data)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No data file found")
+    else:
+        if user_needs in file_contents:
+            messagebox.showinfo(title=user_needs, message=f"Email: {file_contents[user_needs]['email']} \n "
+                                                              f"Password: {file_contents[user_needs]['password']}")
+        else:
+            messagebox.showinfo(title="Not Found", message=f"No details for the {user_needs} website")
+
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def add_content():
-    website_name = website_entry.get()
     mail_entered = email_entry.get()
+    website_name = website_entry.get()
+    website_name = website_name.title()
     password = pass_entry.get()
+    new_data = {website_name: {
+        "email": mail_entered,
+        "password": password
+    }}
 
     if len(website_name) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="Please Don't leave any empty fields")
@@ -37,10 +59,20 @@ def add_content():
                                          message=f"Should I save the following:\n "
                                                  f"Email: {mail_entered} \n Password: {password}")
         if is_okay:
-            with open(file="Password_details", mode='a') as data:
-                data.write(f"\n {website_name} || {mail_entered}  || {password} ")
-            website_entry.delete(0, END)
-            pass_entry.delete(0, END)
+            try:
+                with open(file="Password_details.json", mode='r') as data:
+                    old_file = json.load(data)
+
+            except FileNotFoundError:
+                with open(file="Password_details.json", mode='w') as data:
+                    json.dump(new_data, data, indent=4)
+            else:
+                old_file.update(new_data)
+                with open(file="Password_details.json", mode='w') as data:
+                    json.dump(old_file, data, indent=4)
+            finally:
+                website_entry.delete(0, END)
+                pass_entry.delete(0, END)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -59,9 +91,12 @@ canvas.grid(row=0, column=1)
 website_label = Label(text="Website:")
 website_label.grid(row=1, column=0)
 
-website_entry = Entry(width=36)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=21)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
+
+search_button = Button(text="Search", command=find_password, width=15,background="blue", foreground="white")
+search_button.grid(row=1, column=2)
 
 email_label = Label(text="Email/Username:")
 email_label.grid(row=2, column=0)
