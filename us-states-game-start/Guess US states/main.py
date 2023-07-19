52b583b5a80afadb51ed6cb7d127813cf9d9783e
Niyati Savant <CodingTerex18@gmail.com>
2023-07-19 12:13:31 +0530

    Small modification in US states Game

--- us-states-game-start/Guess US states/main.py
+++ us-states-game-start/Guess US states/main.py
@@ -4,10 +4,8 @@ import pandas
 data = pandas.read_csv("50_states.csv")
 state_list = data["state"].to_list()
 answered_states = []
-missing_state = []
 data_dict = data.to_dict()
 
-print(state_list)
 total_states = 50
 guessed_ones = 0
 screen = turtle.Screen()
@@ -23,9 +21,10 @@ while guessed_ones != 50:
     answer = screen.textinput(title=f"{guessed_ones}/{total_states} Correct Guess", prompt="Guess a state")
     answer = answer.title()
     if answer == "Exit":
-        for state in state_list:
-            if state not in answered_states:
-                missing_state.append(state)
+        missing_state = [state for state in state_list if state not in answered_states]
+        # for state in state_list:
+        #     if state not in answered_states:
+        #         missing_state.append(state)
         new_data = pandas.DataFrame(missing_state)
         new_data.to_csv("states_to_learn.csv")
 
